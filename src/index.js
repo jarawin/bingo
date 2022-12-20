5388a78f70039991436b0405d60d9f6568c01d13
jarawin <newja2021@gmail.com>
2022-12-20 13:17:11 +0700

    OK

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ import ResetLocalStorage from "./pages/command/ResetLocalStorage";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-// "react": "^18.2.0",
-// "react-dom": "^18.2.0",
-// "react-scripts": "5.0.1",
+//
+//
+//
 
 root.render(
   <React.StrictMode>
