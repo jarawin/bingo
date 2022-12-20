2f308e9ba914f486f39f216845596a881791cf16
NickP-real <nick_narak@rocketmail.com>
2022-12-20 18:00:17 +0700

    fix(Counter): logic

--- src/pages/Control.js
+++ src/pages/Control.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 import { FaCat } from "react-icons/fa";
 
+const DEFAULT_NUM = 101;
+
 function Control() {
-  const [rndNum, setRndNum] = useState(101);
+  const [rndNum, setRndNum] = useState(DEFAULT_NUM);
   const [numbers, setNumbers] = useState([]);
   const [isRnd, setIsRnd] = useState(false);
   const showNum = useSpring({
@@ -27,7 +29,7 @@ function Control() {
       alert("Randomized 100 numbers!");
       return;
     }
-    setNumbers([...numbers, rndNum]);
+    if (rndNum !== DEFAULT_NUM) setNumbers([...numbers, rndNum]);
 
     let num;
     do {
@@ -41,6 +43,7 @@ function Control() {
   }
 
   function resetNumber() {
+    setRndNum(DEFAULT_NUM);
     setNumbers(() => []);
   }
 
@@ -48,7 +51,7 @@ function Control() {
     <div className="text-center text-xl flex flex-col gap-y-8 max-w-4xl justify-center items-center h-screen mx-auto">
       <FaCat />
       <div className="text-9xl font-bold ">
-        {rndNum !== 101 ? (
+        {rndNum !== DEFAULT_NUM ? (
           <animated.div
             className={`${
               isRnd
@@ -64,7 +67,7 @@ function Control() {
       </div>
       <Button
         className="hover:bg-black/10"
-        onClick={rndNum !== 101 ? handleOnRandomClick : firstRandom}
+        onClick={rndNum !== DEFAULT_NUM ? handleOnRandomClick : firstRandom}
         label="Random Number"
       />
       <div className="font-bold">Randomized Number:</div>
